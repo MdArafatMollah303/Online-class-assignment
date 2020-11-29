61ae6fd2f4532453a824f9aa63444a822e7547dd
unknown <arafatshuvo18@gmail.com>
2020-11-29 12:33:09 +0600

    commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const Footer = () => {
+    return (
+        <div className ='text-center p-2'>
+            <p >Copy Right Included by Arafat</p>
+        </div>
+    );
+};
+
+export default Footer;
